bd101ae5225a79f0b4129356b5f008ff86b9e356
John Fitzgerald (MBA) <usplitu@gmail.com>
2018-04-06 21:21:13 +0100

    bug fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict'
 const boxMethods = require('./boxes')
-const neighbourMethods = require('./neighbour')
-const validate = require('./validate')
+const isNeighbour = require('./neighbour')
+const isValid = require('./validate')
 
 module.exports = {
 	"populateBoxes": boxMethods.populateBoxes,
 	"whichBox": boxMethods.whichBox,
-	"isNeighbour": neighbourMethods.isNeighbour,
-	"isValid": validate.isValid
+	"isNeighbour": isNeighbour,
+	"isValid": isValid
 }
